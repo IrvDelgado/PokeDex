b98d049726c875499b10c26474274f80c598da72
Irving Delgado <irvingdelgado6@gmail.com>
2021-06-29 23:30:30 -0500

    visualizar pokemon detalle

--- PokeDex/View/DetailViewController.swift
+++ PokeDex/View/DetailViewController.swift
@@ -7,16 +7,69 @@
 
 import UIKit
 
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+
 class DetailViewController: UIViewController {
 
     var pokemonmodel: PokemonModel?
     
-    override func viewDidLoad() {
+    @IBOutlet weak var namelbl: UILabel!
+    
+    @IBOutlet weak var imageV: UIImageView!
+    
+    @IBOutlet weak var abilitieslbl: UILabel!
+    
+    @IBOutlet weak var baseexplbl: UILabel!
+    
+    @IBOutlet weak var weightlbl: UILabel!
+    
+    @IBOutlet weak var typeslbl: UILabel!
+    @IBOutlet weak var idlbl: UILabel!
+    @IBOutlet weak var heightlbl: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
        
         if let poke = pokemonmodel{
-            print(poke)
+            //print(poke)
+            imageV.contentMode = .scaleAspectFill
+            imageV.downloaded(from: poke.detail.sprites.front_default)
+            namelbl.text = poke.name
+            
+            abilitieslbl.text = poke.detail.abilities[0].ability.name
+            
+            baseexplbl.text = String(poke.detail.base_experience)
+            
+            heightlbl.text = String(poke.detail.height)
+            
+            idlbl.text = String(poke.detail.id)
+            
+            typeslbl.text = poke.detail.types[0].type.name
+            
+            weightlbl.text = String(poke.detail.weight)
+            
+            
         }
         else{
             print("Nil")
